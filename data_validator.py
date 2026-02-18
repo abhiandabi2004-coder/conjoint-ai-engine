faff8df0969329910389a55a0ef386ece1586cad
abhiandabi2004-coder <abhiandabi2004@gmail.com>
2026-02-18 19:05:52 +0530

    Update data_validator.py

--- data_validator.py
+++ data_validator.py
@@ -1,12 +1,23 @@
 def validate_format(df, expected_columns):
 
-    if list(df.columns) != expected_columns:
-        return False, f"Column mismatch. Expected: {expected_columns}"
+    # Clean uploaded columns
+    df.columns = df.columns.str.strip()
 
-    if df.isnull().sum().sum() > 0:
-        return False, "Dataset contains missing values."
+    # Clean expected columns
+    expected_columns = [col.strip() for col in expected_columns]
+
+    # Compare as sets (ignore order)
+    if set(df.columns) != set(expected_columns):
+        return False, f"""
+        Column mismatch.
+        Expected: {expected_columns}
+        Found: {list(df.columns)}
+        """
 
     if df["Respondent_ID"].duplicated().any():
         return False, "Duplicate Respondent_ID found."
+
+    if df.isnull().sum().sum() > 0:
+        return False, "Dataset contains missing values."
 
     return True, "Valid format."
